0492ae10d3f40a27380cfb731d5888dfcdabd19a
jlgingrich <jlgingrich@proton.me>
2024-03-06 21:46:46 -0500

    Disabled isort temporarily

--- pittfinder/__init__.py
+++ pittfinder/__init__.py
@@ -1,6 +1,6 @@
 """Minimalist API for https://find.pitt.edu/"""
 
-import pittfinder.api as _api
+from .api import get_search_response, parse_search_response, validate_search_response
 
 
 def search(query):
@@ -16,8 +16,8 @@ def search(query):
         list[dict]: A list of user dictionaries with the returned information.
     """
     # Get raw response and attach HTML parser
-    html = _api.get_search_response(query).text
+    html = get_search_response(query).text
     # Check for and raise API errors
-    _api.validate_search_response(html)
+    validate_search_response(html)
     # Parse the returned html
-    return _api.parse_search_response(html)
+    return parse_search_response(html)
